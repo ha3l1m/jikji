0b2b53b93c19ba28efccfd779bf66c44401ef4d2
chan <jungchan@n3n.co.kr>
2026-03-10 17:24:58 +0900

    문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,20 @@ import { ProductsSection } from '@/components/products-section';
 import { InfraTeaser } from '@/components/infra-teaser';
 import { PricingSnippet } from '@/components/pricing-snippet';
 import { Footer } from '@/components/footer';
+import { I18nProvider } from '@/components/i18n-provider';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black selection:bg-white/30">
-      <Header />
-      <Hero />
-      <ProductsSection />
-      <InfraTeaser />
-      <PricingSnippet />
-      {/* <CTABanner /> */}
-      <Footer />
-    </main>
+    <I18nProvider>
+      <main className="min-h-screen bg-black selection:bg-white/30">
+        <Header />
+        <Hero />
+        <ProductsSection />
+        <InfraTeaser />
+        <PricingSnippet />
+        {/* <CTABanner /> */}
+        <Footer />
+      </main>
+    </I18nProvider>
   );
 }
