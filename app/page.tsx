77de9557e3f64c79ec3a917aa9a994e37c2c0565
chan <jungchan@n3n.co.kr>
2026-03-11 15:23:56 +0900

    /company header 동작 수정

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import { InfraTeaser } from '@/components/infra-teaser';
 import { PricingSnippet } from '@/components/pricing-snippet';
 import { Footer } from '@/components/footer';
 import { I18nProvider } from '@/components/i18n-provider';
+import { ScrollOnMount } from '@/components/scroll-on-mount';
 
 export default function Home() {
   return (
     <I18nProvider>
       <main className="min-h-screen bg-black selection:bg-white/30">
+        <ScrollOnMount />
         <Header />
         <Hero />
         <ProductsSection />
